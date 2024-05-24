79f5dc7b0d749fed1ac919695c8d26037966e470
mirage <themainmirage@gmail.com>
2024-05-24 16:42:14 +0200

    tiny fix

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -6,7 +6,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
-  <link rel="stylesheet" href="../public/css/dashboard/dashboard.css">
+  <link rel="stylesheet" href="/public/css/dashboard/dashboard.css">
+  <link rel="stylesheet" href="/public/css/global.css">
   <script src="/public/js/dashboard.js" defer></script>
 </head>
 <body>
